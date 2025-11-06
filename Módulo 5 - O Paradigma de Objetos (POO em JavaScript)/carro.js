2dfbbe81e4ae0e098699874412662f4d18ef6208
Vítor Machado Coelho <vitor.machaado@gmail.com>
2025-11-06 20:19:03 -0300

    feat(modulo-05): Conclui o laboratório da classe Carro

---
+++ Módulo 5 - O Paradigma de Objetos (POO em JavaScript)/carro.js
@@ -0,0 +1,23 @@
+class Carro {
+
+    constructor(marca, modelo) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ligado = false;
+    }
+
+    ligar() {
+        this.ligado = true;
+        console.log('Carro ' + this.marca + ' ' + this.modelo + ' ligado')
+    }
+
+
+    desligar() {
+        this.ligado = false;
+        console.log('Carro ' + this.marca + ' ' + this.modelo + ' desligado')
+    }
+}
+
+const meuFusca = new Carro("Volkswagen", "Fusca");
+meuFusca.ligar()
+meuFusca.desligar();
