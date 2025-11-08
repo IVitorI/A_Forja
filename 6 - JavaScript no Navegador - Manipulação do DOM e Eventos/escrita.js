720d3c485ac814151eb46868bb6ed5d6b6f652ec
Vítor Machado Coelho <vitor.machaado@gmail.com>
2025-11-08 11:44:18 -0300

    feat(modulo-06): Conclui laboratório de escrita no DOM

---
+++ 6 - JavaScript no Navegador - Manipulação do DOM e Eventos/escrita.js
@@ -0,0 +1,10 @@
+const titulo = document.getElementById("titulo-principal");
+const segundoCard = document.querySelectorAll(".card")[1];
+const primeiroCard = document.querySelector(".card");
+
+
+
+titulo.textContent = "Módulo 6 Dominado";
+segundoCard.innerHTML = "<h3>Produto Esgotado</h3>";
+primeiroCard.setAttribute("style", "background-color:lightblue");
+
---
+++ 6 - JavaScript no Navegador - Manipulação do DOM e Eventos/seletores.js
@@ -0,0 +1,9 @@
+const titulo = document.getElementById("titulo-principal");
+const conttodosOsCards = document.getElementsByClassName("card");
+const primeiroParagrafo = document.querySelector(".card p");
+const todosOsH2 = document.querySelectorAll("h2");
+
+console.log(titulo);
+console.log(conttodosOsCards);
+console.log(primeiroParagrafo);
+console.log(todosOsH2);
